3ff8bdc9012b4a7fdfd3911c6901f18fd8f89172
qwas4689 <qwas4689@gmail.com>
2022-09-22 20:32:47 +0900

    [Feat] : 옵저버 카메라 전환

---
+++ Assets/Scripts/Telescope.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telescope : MonoBehaviour
+{
+    private Vector3 observerCameraPosition;
+
+    void Awake()
+    {
+        observerCameraPosition = new Vector3(0f, 15f, 0f);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                other.gameObject.transform.GetChild(0).position = observerCameraPosition;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
