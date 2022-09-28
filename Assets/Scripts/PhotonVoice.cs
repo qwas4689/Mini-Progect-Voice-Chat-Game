f69fac517d773f8a4746b5ee950aa5ffe63de874
yejin9337 <yejin9337@naver.com>
2022-09-28 10:30:11 +0900

    [Fix]마이크 ON/OFF시 이미지 출력

--- Assets/Scripts/PhotonVoice.cs
+++ Assets/Scripts/PhotonVoice.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Voice;
 using Photon.Voice.Unity;
+using UnityEngine.UI;
 
 public class PhotonVoice : MonoBehaviour
 {
     [SerializeField]
     private Recorder photonVoice;
+    [SerializeField]
+    private GameObject speakerImage;
+
     void Start()
     {
         photonVoice.TransmitEnabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.K))
         {
             photonVoice.TransmitEnabled = true;
+            speakerImage.SetActive(true);
         }
-        if(Input.GetKeyUp(KeyCode.K))   
+        if(Input.GetKeyUp(KeyCode.K))
         {
             photonVoice.TransmitEnabled = false;
+            speakerImage.SetActive(false);
         }
-        
+
     }
 }
