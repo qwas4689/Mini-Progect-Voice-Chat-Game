81cfa50fc0180b088a140d66d9d2314aca594c0c
qwas4689 <qwas4689@gmail.com>
2022-10-06 17:51:19 +0900

    [Fix] Buttons 수정
    버튼스크립트들 합체 및 불필요한 Prefab들 제거

--- Assets/Scripts/UI/Buttons.cs
+++ Assets/Scripts/UI/Buttons.cs
@@ -6,10 +6,56 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
+    // 목표 UI
+    [SerializeField]
+    private GameObject _goalUI;
+
+    // 메뉴 UI
+    [SerializeField]
+    private GameObject _manualUI;
+
+    // UI상호작용 사운드
+    [SerializeField]
+    private AudioSource _exitButtonSound;
+
+    // 메뉴 GameObject
+    [SerializeField]
+    private GameObject _menuUI;
+
+
     public void OnClickReStartButton()
     {
         Time.timeScale = 1f;
         PhotonNetwork.LeaveRoom();
         SceneManager.LoadScene("Lobby");
+    }
+
+    public void OnClickExitGoalUIButton()
+    {
+        _exitButtonSound.Play();
+        _goalUI.SetActive(false);
+    }
+
+    public void OnClickExitManualUIButton()
+    {
+        _exitButtonSound.Play();
+        _manualUI.SetActive(false);
+    }
+
+    public void OnClickExitMenuUIButton()
+    {
+        _exitButtonSound.Play();
+        _menuUI.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OnClickMenuButton()
+    {
+        _exitButtonSound.Play();
+        _menuUI.SetActive(true);
     }
 }
